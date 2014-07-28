a0dcb8446e6cccf6bea68259b219c2202cb54d2a
Omri Gildor <omrigildor@gmail.com>
2014-07-28 12:29:40 -0600

    adding /hello-world and /hello-name

--- src/flasktest.py
+++ src/flasktest.py
@@ -3,14 +3,19 @@ import flask, flask.views
 app = flask.Flask(__name__)
 app.name = "hello-world"
 
-class View(flask.views.MethodView):
+class HelloName(flask.views.MethodView):
     def get(self):
         return flask.render_template('index.html')
 
     def post(self):
-        return "Hello " + str(flask.request.form['go'])
+        return "Hello " + str(flask.request.form['GO'])
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=["GET","POST"])
+class HelloWorld(flask.views.MethodView):
+    def get(self):
+        return "Hello World"
+
+app.add_url_rule('/hello-world', view_func=HelloWorld.as_view('main'),methods=["GET"])
+app.add_url_rule('/hello-name', view_func=HelloName.as_view('name'), methods=["GET","POST"])
 
 app.debug = True
 app.run()
