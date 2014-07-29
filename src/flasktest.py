3b7553aa55ccfe975fa3b953f9231f0aea1980c0
Omri Gildor <omrigildor@gmail.com>
2014-07-29 11:27:53 -0600

    Used a catch_all to grab the name parameter. Default localhost returns Hello World /Hello-Omri returns Hello Omri

--- src/flasktest.py
+++ src/flasktest.py
@@ -3,19 +3,16 @@ import flask, flask.views
 app = flask.Flask(__name__)
 app.name = "hello-world"
 
-class HelloName(flask.views.MethodView):
-    def get(self):
-        return flask.render_template('index.html')
-
-    def post(self):
-        return "Hello " + str(flask.request.form['GO'])
-
-class HelloWorld(flask.views.MethodView):
-    def get(self):
+@app.route("/", defaults={'path':''})
+@app.route("/Hello-<path:path>")
+def catch_all(path):
+    if path == "":
         return "Hello World"
+    else:
+        return "Hello " + path
 
-app.add_url_rule('/hello-world', view_func=HelloWorld.as_view('main'),methods=["GET"])
-app.add_url_rule('/hello-name', view_func=HelloName.as_view('name'), methods=["GET","POST"])
+def get():
+    return catch_all()
 
 app.debug = True
 app.run()
